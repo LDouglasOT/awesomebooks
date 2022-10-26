9bedd2a468c1798bb62a922e09d036871c334f43
Luzinda Douglas <douglas.luzinda.ot@gmail.com>
2022-10-26 18:18:27 +0300

    Add book to array functionality

--- index.js
+++ index.js
@@ -1,60 +1,57 @@
-const booksWrapper = document.querySelector(".books-wrapper");
-const singleBook = document.querySelectorAll(".single-book");
+const booksWrapper = document.querySelector('.books-wrapper');
+const titleField = document.querySelector('.title');
+const autherField = document.querySelector('.auther');
+const addBook = document.querySelector('.add-book');
 
-let dataBooks = [
-  {
-    id: 0,
-    title: "Feel free to remove me!",
-    auther: "Mo Khaled",
-  },
+let booksData = [
   {
     id: 1,
-    title: "Feel free to remove me!",
-    auther: "Luzinda Douglas",
+    title: 'Feel free to remove me!',
+    auther: 'Mo Khaled',
   },
 ];
 
-const ul = document.createElement("ul");
-
-const booksGenerator = () => {
-  dataBooks.map((book) => {
-    let li = document.createElement("li");
-    li.innerHTML = `
-    <h2>${book.title}</h2>
-    <h2>${book.auther}</h2>
-    <button class="remove-btn">Remove</button>
-    `;
-    ul.appendChild(li);
-  });
-};
-
-singleBook.forEach((book, i) => {
-  dataBooks.push(book);
-});
-booksGenerator();
-const maindiv = document.querySelector(".books-wrapper").appendChild(ul);
-
-const add=document.querySelector(".add");
-add.addEventListener("click",()=>{
- let title= document.querySelector(".title").value
- let auther= document.querySelector(".author").value
- let li = document.createElement("li");
- li.innerHTML = `
- <h2>${title}</h2>
- <h2>${auther}</h2>
- <button class="remove-btn">Remove</button>
- `;
- const maindiv = document.querySelector(".books-wrapper ul").appendChild(li);
- 
-console.log(window.localStorage.getItem("myObject"))
-
- const close=document.querySelectorAll(".remove-btn")
-for(let i=0;i<close.length;i++){
-
-  close[i].addEventListener("click",()=>{
-    close[i].parentElement.remove();
-  })
+if (localStorage.getItem('books')) {
+  booksData = JSON.parse(localStorage.getItem('books'));
 }
-})
 
+booksData.forEach((book) => {
+  booksWrapper.innerHTML += `
+           <li class="single-book" data-id=${book.id}>
+              <h2>${book.title}</h2>
+              <h3>${book.auther}</h3>
+              <button class="del-btn">Remove</button>
+           </li>
+    `;
+});
 
+addBook.addEventListener('click', () => {
+  if (titleField !== '' && autherField !== '') {
+    addBooksToArray(titleField.value, autherField.value);
+    titleField.value = '';
+    autherField.value = '';
+  }
+});
+
+// Delete logic
+booksWrapper.addEventListener('click', (e) => {
+  if (e.target.classList.contains('del-btn')) {
+    // Remove from local storage
+    deleteBookFromLs(e.target.parentElement.getAttribute('data-id'));
+    // Remove the item from dom
+    e.target.parentElement.remove();
+  }
+});
+
+const addBooksToArray = (title, auther) => {
+  const book = {
+    id: Date.now(),
+    title,
+    auther,
+  };
+  booksData.push(book);
+  // Add books to Dom
+  addBooksToDom(booksData);
+  // Add books to local storage
+  addDataToLocalStorage(booksData);
+};
